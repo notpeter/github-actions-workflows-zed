8ffa2ac9a16e88e532632ef133b813db774b0211
Peter Tripp <petertripp@gmail.com>
2026-04-24 14:05:27 -0400

    Remove github-yaml-language-server

--- src/github_yaml.rs
+++ src/github_yaml.rs
@@ -1,58 +1,8 @@
-use std::{env, fs};
 use zed_extension_api::{self as zed, serde_json, settings::LspSettings, Result};
 
-const PACKAGE_NAME: &str = "yaml-language-server";
-const SERVER_PATH: &str = "node_modules/yaml-language-server/bin/yaml-language-server";
+struct GithubActionsExtension {}
 
-struct GithubActionsExtension {
-    did_find_server: bool,
-}
-
-impl GithubActionsExtension {
-    fn server_exists(&self) -> bool {
-        fs::metadata(SERVER_PATH).map_or(false, |stat| stat.is_file())
-    }
-
-    fn server_script_path(&mut self, server_id: &zed::LanguageServerId) -> Result<String> {
-        let server_exists = self.server_exists();
-        if self.did_find_server && server_exists {
-            return Ok(SERVER_PATH.to_string());
-        }
-
-        zed::set_language_server_installation_status(
-            server_id,
-            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
-        );
-        let version = zed::npm_package_latest_version(PACKAGE_NAME)?;
-
-        if !server_exists
-            || zed::npm_package_installed_version(PACKAGE_NAME)?.as_ref() != Some(&version)
-        {
-            zed::set_language_server_installation_status(
-                server_id,
-                &zed::LanguageServerInstallationStatus::Downloading,
-            );
-            let result = zed::npm_install_package(PACKAGE_NAME, &version);
-            match result {
-                Ok(()) => {
-                    if !self.server_exists() {
-                        Err(format!(
-                            "installed package '{PACKAGE_NAME}' did not contain expected path '{SERVER_PATH}'",
-                        ))?;
-                    }
-                }
-                Err(error) => {
-                    if !self.server_exists() {
-                        Err(error)?;
-                    }
-                }
-            }
-        }
-
-        self.did_find_server = true;
-        Ok(SERVER_PATH.to_string())
-    }
-}
+impl GithubActionsExtension {}
 
 fn merge(a: &mut serde_json::Value, b: serde_json::Value) {
     match (a, b) {
@@ -67,36 +17,18 @@ fn merge(a: &mut serde_json::Value, b: serde_json::Value) {
 
 impl zed::Extension for GithubActionsExtension {
     fn new() -> Self {
-        Self {
-            did_find_server: false,
-        }
-    }
-
-    fn language_server_command(
-        &mut self,
-        language_server_id: &zed::LanguageServerId,
-        _worktree: &zed::Worktree,
-    ) -> Result<zed::Command> {
-        let server_path = self.server_script_path(language_server_id)?;
-        Ok(zed::Command {
-            command: zed::node_binary_path()?,
-            args: vec![
-                env::current_dir()
-                    .unwrap()
-                    .join(&server_path)
-                    .to_string_lossy()
-                    .to_string(),
-                "--stdio".to_string(),
-            ],
-            env: Default::default(),
-        })
+        Self {}
     }
 
     fn language_server_workspace_configuration(
         &mut self,
-        _language_server_id: &zed::LanguageServerId,
+        language_server_id: &zed::LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<Option<serde_json::Value>> {
+        let lsp_name = language_server_id.as_ref();
+        if lsp_name != "yaml-language-server" {
+            return Ok(None);
+        }
         let mut yaml_settings = serde_json::json!({
             "schemas": {
                 "https://json.schemastore.org/github-workflow.json": [
@@ -138,8 +70,7 @@ impl zed::Extension for GithubActionsExtension {
             "format": { "enable": false }
         });
 
-        if let Ok(lsp_settings) = LspSettings::for_worktree("github-yaml-language-server", worktree)
-        {
+        if let Ok(lsp_settings) = LspSettings::for_worktree(lsp_name, worktree) {
             if let Some(user_settings) = lsp_settings.settings {
                 if let Some(user_yaml) = user_settings.get("yaml").cloned() {
                     merge(&mut yaml_settings, user_yaml);
